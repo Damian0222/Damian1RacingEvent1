5bc70b9f44d22a7ad73a0e60ec563551674d1f0a
Damian0222 <d00229247@student.dkit.ie>
2024-10-09 15:24:46 +0100

    Add project files.

---
+++ Damian1RacingEvent1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Damian1RacingEvent1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
