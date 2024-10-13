c11d8bc0d6f7ad490ca40627a8d8a15dfbe08685
Damian0222 <d00229247@student.dkit.ie>
2024-10-13 19:16:32 +0100

    Created race event class, added a List adn created setters and getters - RaceEvent class

---
+++ Damian1RacingEvent1/Event.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Damian1RacingEvent1
+{
+    public abstract class Event
+    {
+        public string name;
+        public string location;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+        public Event(string name, string location)
+        {
+            Name = name;
+            Location = location;
+        }
+    }
+
+
+}
---
+++ Damian1RacingEvent1/RaceEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Damian1RacingEvent1
+{
+    class RaceEvent : Event
+    {
+
+        public List<Race> races = new List<Race>();
+        public RaceEvent(string name, string location) : base(name, location) { }
+
+        public List<Race> Races
+        {
+            get { return races; }
+            set { races = value; }
+        }
+
+    }
+}
