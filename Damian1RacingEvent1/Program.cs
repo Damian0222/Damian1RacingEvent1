c28912c7f60c257100f48e32b83db97ddbe4d1f4
Damian0222 <d00229247@student.dkit.ie>
2024-10-16 23:27:33 +0100

    Created a createRaceEv, the manager will be able to name and choose the location of the event, if successful it will print out the input details . Created the app and ran it - RacecourseMan/Program class

--- Damian1RacingEvent1/Program.cs
+++ Damian1RacingEvent1/Program.cs
@@ -1,12 +1,44 @@
-﻿namespace Damian1RacingEvent1
+﻿using System.Runtime.CompilerServices;
+
+namespace Damian1RacingEvent1
 {
-    internal class Program
+    class Program
     {
+        static List<RaceEvent> raceEvent = new List<RaceEvent>();
+        static RacecourseMan Rmanager = new RacecourseMan();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.WriteLine($"\nWelcome to the horse racing event!");
+                Console.WriteLine("1. Racecourse manager");
+                Console.WriteLine("Select your role: ");
+                string choice = Console.ReadLine(); ;
 
-            
+                switch (choice)
+                {
+                    case "1":
+                        RmanagerMenu();
+                        break;
+                }
+            }
+        } 
+            static void RmanagerMenu()
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("1. Create a new event");
+                        Console.Write("Enter your choice");
+                        string num = Console.ReadLine();
+
+                        switch (num)
+                        {
+                            case "1":
+                                Rmanager.createRaceEv(raceEvent);
+                                break;
+                        }
+                    }
+                }
+            }
         }
-    }
-}
+    
--- Damian1RacingEvent1/RacecourseMan.cs
+++ Damian1RacingEvent1/RacecourseMan.cs
@@ -8,6 +8,18 @@ namespace Damian1RacingEvent1
 {
     public class RacecourseMan
     {
-        //public void createEvent(List<RaceEvent>);
+       public void createRaceEv(List<RaceEvent> raceEv)
+        {
+            Console.WriteLine("Create the name of your event: ");
+            string Ename = Console.ReadLine();
+            Console.WriteLine("Enter the location of your event: ");
+            string eventloc = Console.ReadLine();
+
+            RaceEvent newEv = new RaceEvent(Ename,eventloc);
+            raceEv.Add(newEv);
+           
+            Console.WriteLine($"New event '{Ename}' has been created in '{eventloc}'.");
+
+        }
     }
 }
