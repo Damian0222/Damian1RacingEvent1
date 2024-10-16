1e598c1be49ed9ff2407bdba548c55387b5cd184
Damian0222 <d00229247@student.dkit.ie>
2024-10-17 00:30:15 +0100

    after selecting the specific event to add a race, the user will be asked about the name of the race and start time. After that a new object Race is created using the Race class constructor, then it displays both race name and event name if successful.

--- Damian1RacingEvent1/RacecourseMan.cs
+++ Damian1RacingEvent1/RacecourseMan.cs
@@ -8,20 +8,22 @@ namespace Damian1RacingEvent1
 {
     public class RacecourseMan
     {
-       public void createRaceEv(List<RaceEvent> raceEv)
+        public void createRaceEv(List<RaceEvent> raceEv)
         {
             Console.WriteLine("Create the name of your event: ");
             string Ename = Console.ReadLine();
             Console.WriteLine("Enter the location of your event: ");
             string eventloc = Console.ReadLine();
 
-            RaceEvent newEv = new RaceEvent(Ename,eventloc);
+            RaceEvent newEv = new RaceEvent(Ename, eventloc);
             raceEv.Add(newEv);
-           
+
             Console.WriteLine($"New event '{Ename}' has been created in '{eventloc}'.");
 
         }
 
+       
+        
         public void AddRaceeToE(List<RaceEvent> raceEv1)
         {
             if (raceEv1.Count == 0)
@@ -29,11 +31,23 @@ namespace Damian1RacingEvent1
                 Console.WriteLine("No events created yet.");
                 return;
             }
-                Console.WriteLine("Please select an event to add a race to:");
+            Console.WriteLine("Please select an event to add a race to:");
             for (int i = 0; i < raceEv1.Count; i++)
             {
-               Console.WriteLine($"{i+1}: {raceEv1[i].Name} at {raceEv1[i].Location}");
+                Console.WriteLine($"{i + 1}: {raceEv1[i].Name} at {raceEv1[i].Location}");
             }
+            int info = int.Parse(Console.ReadLine()) - 1;
 
+            Console.Write("The name of the race: ");
+            string raceName = Console.ReadLine();
+            Console.Write("Choose the start time (yyyy-mm-dd hh:mm");
+            DateTime raceStart = DateTime.Parse(Console.ReadLine());
+            Race newR = new Race(raceName, raceStart);
+            raceEv1[info].AddRace(newR);
+
+            Console.WriteLine($"Race'{raceName}' added to event' {raceEv1[info].Name}'.");
 
         }
+
+    }
+}
