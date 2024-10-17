db670abb61b053dea4823aeb82af4f20c1ec77bf
Damian0222 <d00229247@student.dkit.ie>
2024-10-17 16:06:12 +0100

    Created Racegoer class, viewUpomingEv() will display all of the events created by the racemanger and let you choose which one to view - Racegoer class

--- Damian1RacingEvent1/Program.cs
+++ Damian1RacingEvent1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
 
 namespace Damian1RacingEvent1
 {
@@ -6,12 +7,14 @@ namespace Damian1RacingEvent1
     {
         static List<RaceEvent> raceEvent = new List<RaceEvent>();
         static RacecourseMan Rmanager = new RacecourseMan();
+        static Racegoer Rgoer = new Racegoer();
         static void Main(string[] args)
         {
             while (true)
             {
                 Console.WriteLine($"\nWelcome to the horse racing event!");
                 Console.WriteLine("1. Racecourse manager");
+                Console.WriteLine("2. Racecgoer");
                 Console.WriteLine("Select your role: ");
                 string choice = Console.ReadLine(); ;
 
@@ -22,31 +25,49 @@ namespace Damian1RacingEvent1
                         break;
                 }
             }
-        } 
-            static void RmanagerMenu()
-                {
-                    while (true)
-                    {
-                        Console.WriteLine("1. Create a new event");
-                        Console.WriteLine("2. Add races to event");
-                        Console.WriteLine("3. Add horses to a race");
-                        Console.Write("Enter your choice");
+        }
+        static void RmanagerMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Create a new event");
+                Console.WriteLine("2. Add races to event");
+                Console.WriteLine("3. Add horses to a race");
+                Console.Write("Enter your choice");
                 string num = Console.ReadLine();
 
-                        switch (num)
-                        {
-                            case "1":
-                                Rmanager.createRaceEv(raceEvent);
-                                break;
-                            case "2":
-                                Rmanager.AddRaceeToE(raceEvent);
+                switch (num)
+                {
+                    case "1":
+                        Rmanager.createRaceEv(raceEvent);
                         break;
-                             case "3":
-                                Rmanager.AddhorsesToR(raceEvent);
+                    case "2":
+                        Rmanager.AddRaceeToE(raceEvent);
                         break;
-                }
-                    }
+                    case "3":
+                        Rmanager.AddhorsesToR(raceEvent);
+                        break;
                 }
             }
         }
-    
+        static void RGoerMenu()
+        {
+            while (true)
+            {
+
+                Console.WriteLine("\nRacegoer Menu");
+                Console.WriteLine("1. View all upcoming events");
+                Console.Write("\nEnter your choice (1-2): ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Rgoer.viewUpcomingEv(raceEvent);
+                        break;
+                }
+            }
+        }
+
+    }
+}
---
+++ Damian1RacingEvent1/Racegoer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Damian1RacingEvent1
+{
+    public class Racegoer
+    {
+    public void viewUpcomingEv(List<RaceEvent> raceEve4)
+        { 
+        if( raceEve4.Count == 0)
+           {
+                Console.WriteLine("\nNo events created yet");
+                return;
+            }
+            Console.WriteLine("\n Upcoming race events:");
+            for(int i = 0; i < raceEve4.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {raceEve4[i].Name} at {raceEve4[i].Location}");
+
+            }
+            Console.WriteLine("Select a event");
+            if(int.TryParse(Console.ReadLine(), out int choice) && choice > 0 && choice <= raceEve4.Count) {
+
+
+}
